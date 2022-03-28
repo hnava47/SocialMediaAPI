7cb90e0c65a6bb6ca1241412e94ef7ea44dea255
Hector Nava <hector.r.nava44@gmail.com>
2022-03-28 14:37:50 -0600

    Init repo

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const thoughtRoutes = require('./thought');
+const userRoutes = require('./user');
+
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+
+
+module.exports = router;
