f95570628c37c09609d96f6cef37c209db996a30
Hector Nava <hector.r.nava44@gmail.com>
2022-03-31 12:37:32 -0600

    Updated mongodb connection with separate config file

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/socialMediaAPI')
-    .then(async () => {
-        console.log('Successfully connected to MongoDB');
-    }).catch(error => console.log(error));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+db.once('open', () => {
+    app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+});
